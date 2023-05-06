33eb475977cbc2112ca28b567572457f0d8691bf
nilteras <niltonrnascimento@hotmail.com>
2023-05-06 19:41:17 -0300

    versão com alterar user

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,13 +1,43 @@
+import { useState } from "react";
+
+
+
 export default function User() {
+
+  let [nameUser, setNome] = useState("catanacomics");
+  let [imageUser, setImagem] = useState("assets/img/catanacomics.svg")
+
+  function alterarUser (){
+
+    let novoUser = prompt("Para qual nome deseja alterar?");
+     if(novoUser != null && novoUser != undefined && novoUser != "" && novoUser != " "){
+      setNome(novoUser);
+     } else {
+      alert("Campo vazio ou inválido, tente novamente!");
+     }
+  }
+
+  function alteraImageUser (){
+    let novaImage = prompt("Link da nova imagem por favor: ");
+    if(novaImage != null && novaImage != undefined && novaImage != "" && novaImage != " "){
+      setImagem(novaImage);
+     } else {
+      alert("Campo vazio ou inválido, tente novamente!");
+     }
+  }
+
+
     return (
         <div className="usuario">
-          <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+          <img data-test="profile-image" onClick={alteraImageUser} src={imageUser} alt="imagem de perfil"/>
           <div data-test="name" className="texto">
             <span>
-              <strong>catanacomics</strong>
-              <ion-icon data-test="edit-name" name="pencil"></ion-icon>
+              <strong>{nameUser}</strong>
+              <ion-icon data-test="edit-name" onClick={alterarUser} name="pencil"></ion-icon>
             </span>
           </div>
         </div>
     );
 }
+
+
