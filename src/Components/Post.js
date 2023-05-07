62ae39701156bdba2fb07cd532b0438eb20cd358
nilteras <niltonrnascimento@hotmail.com>
2023-05-07 13:31:31 -0300

    versão com dados dinâmicos

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,9 +1,44 @@
-export default function Post( props ){
-    return (
-      <div data-test="post" className="post">
+import { useState } from "react";
+
+
+
+export default function Post(props) {
+
+  const [bookmark, setBookMark] = useState('bookmark-outline');
+  const [numberLikes, setNumberLikes] = useState(props.likes);
+  const [likeState, setLikeState] = useState({name: "heart-outline", class: ""});
+
+  function isSave() {
+    if (bookmark === "bookmark") {
+      setBookMark("bookmark-outline");
+      return;
+    }
+    setBookMark("bookmark");
+  }
+
+  function clickLike() {
+    if (likeState.name !== "heart") {
+      setLikeState({ name: "heart", red: "red-heart" });
+      setNumberLikes(numberLikes + 1);
+    }
+  }
+
+  function toggleLike() {
+    if (likeState.name === "heart") {
+      setLikeState({ name: "heart-outline", red: "" })
+      setNumberLikes(numberLikes - 1);
+      return;
+    }
+    setLikeState({ name: "heart", red: "red-heart" });
+    setNumberLikes(numberLikes + 1);
+  }
+
+
+  return (
+    <div data-test="post" className="post">
       <div className="topo">
         <div className="usuario">
-          <img src={props.image} alt={props.user}/>
+          <img src={props.image} alt={props.user} />
           {props.user}
         </div>
         <div className="acoes">
@@ -12,28 +47,28 @@ export default function Post( props ){
       </div>
 
       <div className="conteudo">
-        <img data-test="post-image" src={props.imagePost} alt=""/>
+        <img data-test="post-image" src={props.imagePost} onClick={clickLike} alt="" />
       </div>
 
       <div className="fundo">
         <div className="acoes">
           <div>
-            <ion-icon data-test="like-post" name="heart-outline"></ion-icon>
+            <ion-icon data-test="like-post" class={likeState.red} onClick={toggleLike} name={likeState.name}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon data-test="save-post" name="bookmark-outline"></ion-icon>
+            <ion-icon data-test="save-post" onClick={isSave} name={bookmark}></ion-icon>
           </div>
         </div>
 
         <div className="curtidas">
-          <img src={props.likeImage} alt=""/>
+          <img src={props.likeImage} alt="" />
           <div className="texto">
-            Curtido por <strong>{props.likeName}</strong> e <strong data-test="likes-number" >outras {props.likes}</strong>
+            Curtido por <strong>{props.likeName}</strong> e <strong data-test="likes-number" >outras {numberLikes}</strong>
           </div>
         </div>
       </div>
-      </div>
-    );
+    </div>
+  );
 }
